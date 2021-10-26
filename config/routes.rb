1e774b45c0faa04d7f58257ab71de66dc3ec1c08
SizweMalobola <25reyals@gmail.com>
2021-10-26 14:03:37 +0200

    set up routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'events#index'
   resources :events
   devise_for :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
