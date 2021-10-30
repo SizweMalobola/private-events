b1372afadd0b17174110de6bb4b0416b6a714b43
SizweMalobola <25reyals@gmail.com>
2021-10-30 07:43:08 +0200

    added routes for the 'through' table that will allow a user to become an 'attendee' of an event

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :events
   devise_for :users
   resources :users, only: [:show]
+  resources :event_attendances, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
