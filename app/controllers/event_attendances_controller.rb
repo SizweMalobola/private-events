9ff303d71bedd5d067ffaf4d80cf733d1594a74f
SizweMalobola <25reyals@gmail.com>
2021-11-02 11:55:17 +0200

    created an interface in the show view where the user can indicate that they want ot attend an event

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,2 +1,18 @@
 class EventAttendancesController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        @attendee = current_user.event_attendances.new(event_id: params[:event_id])
+        if @attendee.save
+            flash[:success] = "You have successfully set you attendance to an event"
+            redirect_to root_path
+        else
+            flash[:danger] = "Shit went horribly wrong"
+            redirect_to root_path
+        end
+    end
+
+    def destroy
+    end
+
 end
