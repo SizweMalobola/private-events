8ac6424784feeeefa5022df2a599861cf1a96da2
SizweMalobola <25reyals@gmail.com>
2021-11-07 15:36:16 +0200

    added validations to the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,13 @@ class Event < ApplicationRecord
     has_many :event_attendances
     has_many :attendees, through: :event_attendances, source: :attendee
     has_one_attached :event_image
+    validates :title, presence: true, length: {minimum: 8}
+    validates :description, presence: true, length: {maximum: 500}
+    validates :event_image, presence: true
+    validates :starting_date, presence: true
+    validates :ending_date, presence: true
+    validates :starting_time, presence: true
+    validates :ending_time, presence: true
+    validates :location, presence: true, length: {minimum: 8}
+    validates :user_id, presence: true
 end
