88f8fb4eacb781bfb16442626d46eac306cfbc1a
SizweMalobola <25reyals@gmail.com>
2021-11-09 14:31:16 +0200

    defined a helper method that will dynamically display the sites title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # Returns the full tile on a per-page basis
+    def full_title(title)
+        base_title = "Event Horizon - Your number one event management solution"
+        if title.empty?
+            base_title
+        else
+            "#{title} | #{base_title}"
+        end
+    end
 end
